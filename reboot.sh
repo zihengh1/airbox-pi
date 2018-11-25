28987534a0ebb2d49619382938ff20d652fb9733
zihengh1 <105703031@nccu.edu.tw>
2018-11-25 12:15:53 +0800

    modify rebbot

--- reboot.sh
+++ reboot.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 sleep 5
-[-f /home/pi/Rescue_CA/test.py ] && {
-    # /usr/bin/git -C /home/pi/SensorArray fetch origin
-    # /usr/bin/git -C /home/pi/SensorArray reset --hard origin/master
-    sudo pigpiod
-    nohup python -u /home/pi/Rescue_CA/test.py &
-} 
-
-    # || {
-    # /usr/bin/git clone https://github.com/zihengh1/SensorArray/ /home/pi/SensorArray
-    # nohup python -u /home/pi/SensorArray/sensor.py &
-    # }
+[ -f /home/pi/airbox-pi/PiM25.py ] && {
+    /usr/bin/git -C /home/pi/airbox-pi fetch origin
+    /usr/bin/git -C /home/pi/airbox-pi reset --hard origin/master
+    /usr/bin/python -u /home/pi/airbox-pi/PiM25.py & > ans.txt
+} || {
+    /usr/bin/git clone https://github.com/zihengh1/airbox-pi/ /home/pi/airbox-pi
+    /usr/bin/python -u /home/pi/airbox-pi/PiM25.py & > ans.txt
+}
