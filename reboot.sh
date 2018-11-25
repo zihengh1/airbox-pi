488bb1046f12b6a410c2cf944269b2e6fea76088
Tzu-Heng, Huang <35376020+zihengh1@users.noreply.github.com>
2018-11-25 12:41:08 +0800

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -4,8 +4,8 @@ sleep 5
 [ -f /home/pi/airbox-pi/PiM25.py ] && {
     /usr/bin/git -C /home/pi/airbox-pi fetch origin
     /usr/bin/git -C /home/pi/airbox-pi reset --hard origin/master
-    /usr/bin/python -u /home/pi/airbox-pi/PiM25.py &>/dev/null
+    /usr/bin/python -u /home/pi/airbox-pi/PiM25.py &> ans.txt
 } || {
     /usr/bin/git clone https://github.com/zihengh1/airbox-pi/ /home/pi/airbox-pi
-    /usr/bin/python -u /home/pi/airbox-pi/PiM25.py &>/dev/null
+    /usr/bin/python -u /home/pi/airbox-pi/PiM25.py &> ans2.txt
 }
