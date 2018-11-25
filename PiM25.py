8aa6cad5af6c998223f2262196471be47a9b241e
zihengh1 <105703031@nccu.edu.tw>
2018-11-25 09:03:28 +0000

    add upload data function

--- PiM25.py
+++ PiM25.py
@@ -2,6 +2,8 @@ import time
 import pigpio
 import commands
 from datetime import datetime
+import PiM25_config as Conf
+import os
 
 def bytes2hex(s):
     return "".join("{:02x}".format(c) for c in s)
@@ -17,13 +19,30 @@ def data_read(dstr):
         return weather
     else:
         data_slice = dstr[index : index + data_len]
-        weather += '|PM1_0:%d' % (int(data_slice[16] + data_slice[17] + data_slice[18] + data_slice[19], 16))       # pm1
-        weather += '|PM2_5:%d' % (int(data_slice[20] + data_slice[21] + data_slice[22] + data_slice[23], 16))       # pm25
-        weather += '|PM_10:%d' % (int(data_slice[24] + data_slice[25] + data_slice[26] + data_slice[27], 16))       # pm10
-        weather += '|Tmp:%d' % (int(data_slice[48] + data_slice[49] + data_slice[50] + data_slice[51], 16) / 10)    # Temperature
-        weather += '|RH:%d' % (int(data_slice[52] + data_slice[53] + data_slice[54] + data_slice[55], 16) / 10)     # Humidity 
+        print(data_slice)
+        weather += '|CFPM1.0=%d' % (int(data_slice[8] + data_slice[9] + data_slice[10] + data_slice[11], 16))        # cf_pm1 
+        weather += '|CFPM2.5=%d' % (int(data_slice[12] + data_slice[13] + data_slice[14] + data_slice[15], 16))      # cf_pm2.5
+        weather += '|CFPM10=%d' % (int(data_slice[16] + data_slice[17] + data_slice[18] + data_slice[19], 16))     # cf_pm10
+        weather += '|s_d2=%d' % (int(data_slice[20] + data_slice[21] + data_slice[22] + data_slice[23], 16))       # pm1
+        weather += '|s_d0=%d' % (int(data_slice[24] + data_slice[25] + data_slice[26] + data_slice[27], 16))       # pm2.5
+        weather += '|s_d1=%d' % (int(data_slice[28] + data_slice[29] + data_slice[30] + data_slice[31], 16))       # pm10
+        weather += '|s_t0=%d' % (int(data_slice[48] + data_slice[49] + data_slice[50] + data_slice[51], 16) / 10)    # Temperature
+        weather += '|s_h0=%d' % (int(data_slice[52] + data_slice[53] + data_slice[54] + data_slice[55], 16) / 10)     # Humidity 
         return weather 
 
+def upload_data(msg):
+    msg += '|app=%s' % (Conf.APP_ID)
+    msg += '|device=%s' % (Conf.DEVICE)
+    msg += '|device_id=%s' % (Conf.DEVICE_ID)
+    msg += '|gps_lat=%s' % (Conf.GPS_LAT)
+    msg += '|gps_lon=%s' % (Conf.GPS_LON)
+    msg += Conf.others
+    
+    Restful_URL = Conf.Restful_URL
+    print(msg)
+    restful_str = "wget -O /tmp/last_upload.log \"" + Restful_URL + "?device_id=" + Conf.DEVICE_ID + "&msg=" + msg + "\""
+    os.system(restful_str)
+    
 RX = 15
 path = "/home/pi/Data/"
 status, process = commands.getstatusoutput('sudo pidof pigpiod')
@@ -61,13 +80,14 @@ while True:
             data_hex = bytes2hex(data)
             weather_data = data_read(data_hex) 
             if len(weather_data):
-                weather_data += '|%s_%s' % (str(now_time[0]), str(now_time[1]))
-                print(weather_data)
+                weather_data += '|date=%s' % (str(now_time[0]))
+                weather_data += '|time=%s' % (str(now_time[1]))
+                upload_data(weather_data)
         else:
             print("read nothing")
  
     except Exception as e:
-        print("close")
+        print(e)
         pi.bb_serial_read_close(RX)
         print("close success")
 
@@ -79,8 +99,8 @@ while True:
             print(e)
             print "Error: writing to SD"    
     pi.bb_serial_read_close(RX)
-    time.sleep(295)
+    time.sleep(5)
 
 pi.stop()
 print("End")
-   
+
