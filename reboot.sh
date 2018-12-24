8ddc22b93d57215abd220124649a08b09b3fc068
Tzu-Heng, Huang <35376020+zihengh1@users.noreply.github.com>
2018-12-24 10:58:32 +0800

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -6,9 +6,9 @@ sleep 5
 [ -f /home/pi/airbox-pi/PiM25.py ] && {
     /usr/bin/git -C /home/pi/airbox-pi fetch origin
     /usr/bin/git -C /home/pi/airbox-pi reset --hard origin/master
-    /usr/bin/nohup python -u /home/pi/airbox-pi/PiM25.py &
+    /usr/bin/sudo nohup python -u /home/pi/airbox-pi/PiM25.py &
 } || {
     /usr/bin/git clone OTA /home/pi/airbox-pi
-    /usr/bin/nohup python -u /home/pi/airbox-pi/PiM25.py &
+    /usr/bin/sudo nohup python -u /home/pi/airbox-pi/PiM25.py &
 }
 
