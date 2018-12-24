a467027b3e473d7bd677357a086f916f180c5f74
Tzu-Heng, Huang <35376020+zihengh1@users.noreply.github.com>
2018-12-24 13:58:23 +0800

    Update PiM25.py

--- PiM25.py
+++ PiM25.py
@@ -238,7 +238,7 @@ while True:
             print(e)
             print "Error: writing to SD"    
     ##############################
-    time.sleep(291)
+    time.sleep(295)
 
 pi.stop()
 print("End")
